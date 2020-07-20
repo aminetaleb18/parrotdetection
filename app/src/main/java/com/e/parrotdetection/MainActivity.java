8aefb6d433ab80afd6cdc0ff39f5581de1c273fb
aminetaleb18 <aminetaleb18@gmail.com>
2020-07-20 14:15:35 +0200

    Initialisation du projet

--- app/src/main/java/com/e/parrotdetection/MainActivity.java
+++ app/src/main/java/com/e/parrotdetection/MainActivity.java
@@ -1,19 +1,17 @@
 package com.e.parrotdetection;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
